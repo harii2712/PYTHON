e37fe5ff8016080e935a9753837d1b31ae40a94d
SRI HARIHARAN V <srihariharan419@gmail.com>
2025-05-19 19:48:20 +0530

    Add files via upload

---
+++ casting.py
@@ -0,0 +1,20 @@
+a=10
+print(a)
+
+a="10"
+b="40"
+c=a+b
+print(c)
+
+
+
+a=int("10")
+b=int("40")
+c=a+b
+print(c)
+
+
+
+a=("kanima direct by karthick subburaj")
+find i in range(a):
+print(a=0,len(a)):
